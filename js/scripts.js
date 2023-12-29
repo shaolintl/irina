7f97c0cde6d23543de30bceb155fdc3640e9e516
Tomer Libal <shaolintl@gmail.com>
2023-12-29 17:39:11 +0100

    youtube

--- js/scripts.js
+++ js/scripts.js
@@ -69,3 +69,39 @@ function switchLanguage(language) {
         }
     }
 }
+
+document.querySelectorAll('#video-carousel .card-img-top').forEach(item => {
+  item.addEventListener('click', event => {
+    const videoId = event.target.getAttribute('data-video-id');
+    document.getElementById('main-video').src = `https://www.youtube.com/embed/${videoId}`;
+  });
+});
+
+function addVideoClickListeners() {
+  document.querySelectorAll('#video-carousel .card-img-top').forEach(item => {
+    item.addEventListener('click', event => {
+      const videoId = event.target.getAttribute('data-video-id');
+      document.getElementById('main-video').src = `https://www.youtube.com/embed/${videoId}`;
+    });
+  });
+}
+
+addVideoClickListeners();
+
+let items = document.querySelectorAll('.carousel .carousel-item')
+
+items.forEach((el) => {
+    const minPerSlide = 4
+    let next = el.nextElementSibling
+    for (var i = 1; i < minPerSlide; i++) {
+        if (!next) {
+            next = items[0]
+        }
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
+    }
+});
+
+// Reattach event listeners to cloned items
+addVideoClickListeners();
